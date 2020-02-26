a182147a9c3a6acc6d6a418bbe543e7835786208
Margot <margaretsobota@gmail.com>
2020-02-26 01:04:47 -0600

    added pin entry button onclick event

---
+++ src/journal.js
@@ -0,0 +1,10 @@
+const pinEntry = () => {
+    alert("Entry is now visible to therapist.");
+    document.getElementById('pin').style.opacity = "1";
+};
+
+
+// const pinEntry = (ev) => {
+// 	document.querySelector("#pin").style.opacity = "1";
+// 	alert("Entry is now visible to therapist.");
+// }
