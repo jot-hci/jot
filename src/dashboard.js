58b58eeda754426239e348538b8c65a5ffcdc260
Margot <margaretsobota@gmail.com>
2020-02-27 16:11:40 -0600

    dynamic journal storage

---
+++ src/dashboard.js
@@ -0,0 +1,19 @@
+
+
+const getEntryTemplate = (data, num) => {
+	
+	const template = `
+	<a href="#" id="${num}">${data.title}</a>
+	`
+	return template;
+
+}
+
+const loadEntries = () => {
+	var entries = document.querySelector(".entries");
+	alert (localStorage.length);
+	for (var i = 0; i < localStorage.length; i++){
+		entries.innerHTML += getEntryTemplate(JSON.parse(localStorage.getItem(localStorage.key(i))), i);
+	}
+
+};
--- src/journal.js
+++ src/journal.js
@@ -1,3 +1,11 @@
+let numEntries = 0;
+let saveCount = 0;
+var ind = 0;
+
+const newEntry = () => {
+	//saveCount = 0;
+}
+
 const pinEntry = () => {
 	const pin = document.getElementById('pin');
 	if (pin.style.opacity == "1")
@@ -11,4 +19,32 @@ const pinEntry = () => {
 	    pin.style.opacity = "1";
 	}
 };
+
+const saveEntry = () => {
+	if (saveCount == 0)
+	{
+		saveCount++;
+		var newEntry = {
+			title: document.querySelector("#title-input").value,
+			content: document.querySelector("#entry").value,
+			mood: document.querySelector(".slider").value,
+		};
+		window.localStorage.setItem(localStorage.length, JSON.stringify(newEntry));
+		ind = localStorage.length;
+		numEntries ++;
+	}
+
+	else {
+		saveCount++;
+		var entry = JSON.parse(window.localStorage.getItem(localStorage.key(ind)));
+		entry = {
+			title: document.querySelector("#title-input").value,
+			content: document.querySelector("#entry").value,
+			mood: document.querySelector(".slider").value,
+		};
+		window.localStorage.setItem(ind, JSON.stringify(entry));
+	}
+
+};
+
 
